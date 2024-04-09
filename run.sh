869c6cdc3e2973e69c5a38c4aca92554a5bdd141
Noah <noah@Noahs-MacBook-Pro-2.local>
2024-04-09 11:41:45 -0400

    npm install added

--- run.sh
+++ run.sh
@@ -4,17 +4,11 @@ python3.12 -m venv env
 source env/bin/activate
 python3 -m pip install -r requirements.txt
 
-# Navigate to the backend directory
 cd backend || exit
-
-# Run the Django development server
 python3 manage.py runserver &
 
-# Navigate back to the parent directory
 cd ..
-
-# Navigate to the frontend directory
 cd frontend || exit
 
-# Run the React development server
+npm install
 npm run dev
